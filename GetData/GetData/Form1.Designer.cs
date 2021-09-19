e9f2f59eaba8e9006c4faa01101ef3a62041b4ae
Philip <74772920+Philip172@users.noreply.github.com>
2021-09-19 15:37:44 +0400

    Add files via upload
    This files are programm which copy data from one excel file to second excel file

---
+++ GetData/GetData/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace GetData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(223, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(299, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Copy Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(15, 145);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(754, 601);
+            this.textBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(167, 75);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(416, 33);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(904, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "excel_mode";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(790, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(384, 30);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "C:\\C#\\FromHere.xlsx";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(790, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(384, 30);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "C:\\C#\\ToHere.xlsx";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(914, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "To:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 761);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GetData/GetData/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Office.Interop.Excel; //Подключаем соответствующую библиотеку
+using System;
+using System.Windows.Forms;
+
+namespace GetData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value++;
+
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application(); //Excel
+            Workbook xlWB; //рабочая книга откуда будем копировать лист  
+            Workbook xlWB2; //рабочая книга куда будем копировать лист
+            Worksheet xlSht; //лист Excel            
+
+            progressBar1.Value++;
+
+            //название файла Excel откуда будем копировать лист
+            xlWB = xlApp.Workbooks.Open(@"C:\C#\FromHere.xlsx");
+
+            progressBar1.Value++;
+
+            //название файла Excel куда будем копировать лист
+            xlWB2 = xlApp.Workbooks.Open(@"C:\C#\ToHere.xlsx");
+
+            progressBar1.Value++;
+
+            //название листа или 1-й лист в книге xlSht = xlWB.Worksheets[1];
+            xlSht = xlWB.Worksheets[1];
+
+            progressBar1.Value++;
+
+            //сам процесс копирования листа из одного файла в другой
+            xlSht.Copy(After: xlWB2.Worksheets[xlWB2.Worksheets.Count]);
+
+            progressBar1.Value++;
+
+            MessageBox.Show("Лист '" + xlSht.Name.ToString() + "' успешно скопирован", "Поиск", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            progressBar1.Value++;
+
+            xlApp.Visible = true; //отображаем Excel
+
+            progressBar1.Value++;
+
+            //xlWB2.Close(true); //закрываем и сохраняем изменения в файле 2            
+
+
+            //xlApp.Quit(); //закрываем Excel
+
+
+
+            progressBar1.Value = progressBar1.Maximum;
+        }
+    }
+}
